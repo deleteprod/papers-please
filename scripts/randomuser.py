63e204e0b1a88309d5a2582e37f9b9c27f07b838
Delete Prod <martynjamesoconnor@hotmail.co.uk>
2022-01-16 09:05:53 +0000

    Initial commit

---
+++ scripts/randomuser.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import json
+import requests
+import pprint
+import time
+
+URL = "https://randomuser.me/api/"
+
+
+def get_user(url=URL,count=2):
+    for counter in range(1,count):
+        result = json.loads(requests.get(url).text)
+
+        marker = result['results'][0]['name']
+        print("\n" + "Result No " + str(counter) + ": " + marker['title'] + " " + marker['first'] + " " + marker['last'])
+        time.sleep(3)
+
+        print("\n")
+
+        output = pprint.pprint(result)
+        print(output)
+
+my_response = get_user()
